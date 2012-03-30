6f09513b8c29029cfdcc26f181c98847250bc5ea
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-03-30 10:18:19 +0000

    added groups to users

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -29,13 +29,28 @@ class base {
 
 
 	#Users - The password hash is taken from /etc/shadow on this VM. UGLY!
+	#Fictional user
 	$bob_pw='$6$hUNOhBMH$U8t1YDPO9mEPhYmkSwiwXn2xbHKdkJgtu710vQAPG.pOse0Fvl1kcXYKyKK4Xqje/NaRodaIsI6e1w8nlxMQS0'
 	user { "bob":
-		ensure => present,
-		shell	=> "/bin/bash",
-		home	=> "/home/bob",
+		ensure     => present,
+		shell	   => "/bin/bash",
+		home	   => "/home/bob",
 		managehome => true,
-		password => "$bob_pw",
+		groups     => 'admin',
+		password   => "$bob_pw",
 	}
+
+	#Olly
+        $ob30_pw='$6$pufwrR.L$0R9625P.e.Plqs4Dj8qMQ7BTf2Hzw5E.uYZQYXkZK.q.2K9oN.f1EddPOA2wMYD6fBhHW1v5GFsXwYhYvSD9B0'
+        user { "ob30":
+                ensure     => present,
+                shell      => "/bin/bash",
+                home       => "/home/ob30",
+                managehome => true,
+                password   => "$ob30_pw",
+        }
+
+
+
 
 }
