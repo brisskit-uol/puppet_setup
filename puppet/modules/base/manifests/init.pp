f61b46c121c402485cf3549ed714e8e33fb31b55
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-04-02 20:30:28 +0000

    loads of edits

--- puppet/modules/base/manifests/init.pp
+++ puppet/modules/base/manifests/init.pp
@@ -40,6 +40,34 @@ class base {
 		password   => "$bob_pw",
 	}
 
+
+	#Make sure we have a .ssh dir to put stuff in
+	file { "/home/bob/.ssh":
+    		ensure  => directory,
+    		owner   => 'bob',
+		group   => 'bob',
+    		mode    => 0700,
+	}
+
+	#Copy the private key across
+	file { "/home/bob/.ssh/id_rsa":
+		ensure => present,
+		owner  => 'bob',
+		group  => 'bob',
+		mode   => 0700,
+		source => "puppet:///modules/base/bru1_bob_id_rsa",
+	}
+
+	$bob_puplic_key='AAAAB3NzaC1yc2EAAAADAQABAAABAQDNTkWBjkd7eoSmo8yE1Mgg9e4s5wvqSMKKdZNfX/HXOFph8KiEBJYhgCs5XLK6hP9yZYaOYgP5Mt5OL3QKjSx/hidY2C5s8DR5xQ3dvxRaaz/8GD+KDsf/0ifQJKaocP3f/EgwXxHm77mA9WNtZ5bnOLeqnWdfMTzg8EpCBeHUwX4nfAZILTWHjad9NHVwN4h2bPbjkGdFf0eMsJM3PISMuyYYp9H1LYu0rXNasiZ+cQasThid95qlBDiqru5tLEA8bbcROArjRHXcLlo4Psh0JGUkcrcfrTuhyAUUH0Gzm5dJQT9BIldubmr3tQSMYACq2RjtHu0ovjWOHU7kNx6X'
+	ssh_authorized_key { "bob-rsa-key":
+   		ensure => 'present',
+   		key => "$bob_public_key",
+   		type => 'rsa',
+   		user => 'bob',
+   		require => File["/home/bob/.ssh"],
+	}
+
+
 	#Olly
         $ob30_pw='$6$pufwrR.L$0R9625P.e.Plqs4Dj8qMQ7BTf2Hzw5E.uYZQYXkZK.q.2K9oN.f1EddPOA2wMYD6fBhHW1v5GFsXwYhYvSD9B0'
         user { "ob30":
