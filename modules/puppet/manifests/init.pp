04ffd3d6c841ef98c14326144978e32b6ed3bf9f
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-04-17 15:44:13 +0000

    make puppet restart not start then stop

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,7 +1,15 @@
 class puppet {
 
+	service { "puppet":
+		ensure     => "running",
+		hasrestart => true,
+	}
+
+
+
 	#Manage the puppet config file
 	file { "/etc/puppet/puppet.conf":
+		notify => Service["puppet"],
 		ensure => present,
 		owner  => 'root',
 		group  => 'root',
