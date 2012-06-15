f0070e056113a4a512b47d84ddce526edae42c2c
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-06-15 14:36:55 +0000

    tidied init file up

--- puppet/modules/users/manifests/init.pp
+++ puppet/modules/users/manifests/init.pp
@@ -1,3 +1,3 @@
 class users {
-	include users::alice, users::bob, users::mike, users::ob30, users::si84, users::ss727, users::bru1_backup
+	include users::ob30, users::si84, users::ss727, users::bru1_backup
 }
