07dd6de4f4fbe74926e5a5d6d9439987b048ba80
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-06-21 09:41:19 +0000

    added demo vapp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -97,6 +97,52 @@ node /^bru3-.*$/ {
 }
 
 
+
+#################################################
+#demo
+#################################################
+#camp - everyone needs to be on this to get
+#to the other VMs
+node 'demo-camp.brisskit.le.ac.uk' {
+        include bru_base
+        include users::jl99, users::rcf8, users::si84, users::ss727, users::tb143
+}
+
+#catissue
+node 'demo-catissue.brisskit.le.ac.uk' {
+        include bru_base
+        include postfix
+        include users::si84, users::ss727
+}
+
+#civicrm
+node 'demo-civicrm.brisskit.le.ac.uk' {
+        include bru_base
+        include postfix
+        include users::rcf8, users::si84, users::ss727, users::tb143
+}
+
+#i2b2
+node 'demo-i2b2.brisskit.le.ac.uk' {
+        include bru_base
+        include users::si84, users::ss727
+}
+
+
+#onyx
+node 'demo-onyx.brisskit.le.ac.uk' {
+        include bru_base
+        include users::jl99
+}
+
+#any other ones that have been missed out
+node /^demo-.*$/ {
+        include bru_base
+}
+
+
+
+
 #################################################
 #BE VERY CAREFUL WITH THE ONES BELOW, THEY ARE
 #THE MANAAGEMENT VMS!
