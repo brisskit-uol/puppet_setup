8d28ccae01c7f0e3b26d20b428a5c7cb2f2920f2
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-06-28 10:22:34 +0000

    added bru1+2 integration users

---
+++ modules/users/manifests/bru3_integration.pp
@@ -0,0 +1,34 @@
+class users::bru3_integration {
+
+	$username="bru3_integration"
+
+	user { $username:
+                ensure     => present,
+                shell      => "/bin/bash",
+                home       => "/home/${username}",
+                managehome => true,
+        }
+
+
+
+        #Make sure we have a home dir to put stuff in
+        file { "/home/${username}":
+                ensure  => directory,
+                owner   => "${username}",
+                group   => "${username}",
+                mode    => '644',
+        }
+
+
+        #Make sure we have a .ssh dir to put stuff in
+	file { "/home/${username}/.ssh":
+   		ensure  => directory,
+  		owner   => "${username}",
+		group   => "${username}",
+		mode    => '644',
+		require => File["/home/${username}"],
+	}
+
+
+
+}
