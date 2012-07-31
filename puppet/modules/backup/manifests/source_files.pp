5c667885575700699880113ec33c69276d787b88
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-07-31 15:26:50 +0000

    lots of backup dev

--- puppet/modules/backup/manifests/source_files.pp
+++ puppet/modules/backup/manifests/source_files.pp
@@ -4,86 +4,123 @@
 
 class backup::source_files {
 
+	#Lets figure out the vApp name
+	$temp1 = split("${fqdn}", '[.]')
+	$hostname = $temp1[0]
+	notify{"Hostname: $hostname":}
+	$temp2 = split("$hostname", '-')
+	$role = $temp2[1]
+	notify{"Role: $role":}
+
 	#############################################################
 	#Customer vapp data
+	case $role
+	{
+		camp:
+		{
+		        #admin.sh script
+		        file { "/var/local/brisskit/backup/source/admin.sh":
+        		        ensure  => file,
+        		        owner   => "root",
+        		        group   => "backup",
+        		        mode    => '650',
+				source  => 'puppet:///modules/backup/customer/admin.sh',
+                		require => File["/var/local/brisskit/backup/source"],
+        		}
 
-        #admin.sh script
-        file { "/var/local/brisskit/backup/source/admin.sh":
-                ensure  => file,
-                owner   => "root",
-                group   => "backup",
-                mode    => '650',
-		source  => 'puppet:///modules/backup/admin.sh',
-                require => File["/var/local/brisskit/backup/source"],
-        }
+		        #vapp_master.sh script
+        		file { "/var/local/brisskit/backup/source/vapp_master.sh":
+                		ensure  => file,
+                		owner   => "root",
+                		group   => "backup",
+                		mode    => '650',
+                		source  => 'puppet:///modules/backup/customer/vapp_master.sh',
+                		require => File["/var/local/brisskit/backup/source"],
+        		}
+		}
+	
+		catissue:
+		{
 
+		        #catissue.sh script
+        		file { "/var/local/brisskit/backup/source/catissue.sh":
+                		ensure  => file,
+                		owner   => "root",
+                		group   => "backup",
+                		mode    => '650',
+                		source  => 'puppet:///modules/backup/customer/catissue.sh',
+                		require => File["/var/local/brisskit/backup/source"],
+        		}
+		}
 
-        #catissue.sh script
-        file { "/var/local/brisskit/backup/source/catissue.sh":
-                ensure  => file,
-                owner   => "root",
-                group   => "backup",
-                mode    => '650',
-                source  => 'puppet:///modules/backup/catissue.sh',
-                require => File["/var/local/brisskit/backup/source"],
-        }
+		civicrm:
+		{
 
+			#civicrm.sh script
+        		file { "/var/local/brisskit/backup/source/civicrm.sh":
+                		ensure  => file,
+                		owner   => "root",
+                		group   => "backup",
+                		mode    => '650',
+				source  => 'puppet:///modules/backup/customer/civicrm.sh',
+                		require => File["/var/local/brisskit/backup/source"],
+        		}
+		}
 
+		onyx:
+		{	
+			#onyx.sh script
+		        file { "/var/local/brisskit/backup/source/onyx.sh":
+                		ensure  => file,
+                		owner   => "root",
+                		group   => "backup",
+                		mode    => '650',
+                		source  => 'puppet:///modules/backup/customer/onyx.sh',
+                		require => File["/var/local/brisskit/backup/source"],
+        		}
 
-	#civicrm.sh script
-        file { "/var/local/brisskit/backup/source/civicrm.sh":
-                ensure  => file,
-                owner   => "root",
-                group   => "backup",
-                mode    => '650',
-		source  => 'puppet:///modules/backup/civicrm.sh',
-                require => File["/var/local/brisskit/backup/source"],
-        }
-
-        #global_master.sh script
-        file { "/var/local/brisskit/backup/source/global_master.sh":
-                ensure  => file,
-                owner   => "root",
-                group   => "backup",
-                mode    => '650',
-		source  => 'puppet:///modules/backup/global_master.sh',
-                require => File["/var/local/brisskit/backup/source"],
-        }
-
-        #onyx.sh script
-        file { "/var/local/brisskit/backup/source/onyx.sh":
-                ensure  => file,
-                owner   => "root",
-                group   => "backup",
-                mode    => '650',
-                source  => 'puppet:///modules/backup/onyx.sh',
-                require => File["/var/local/brisskit/backup/source"],
-        }
-
-        #vapp_master.sh script
-        file { "/var/local/brisskit/backup/source/vapp_master.sh":
-                ensure  => file,
-                owner   => "root",
-                group   => "backup",
-                mode    => '650',
-		source  => 'puppet:///modules/backup/vapp_master.sh',
-                require => File["/var/local/brisskit/backup/source"],
-        }
+        	}
+	}
 
 
 	#############################################################
 	#Global admin
-        #vapp_master.sh script
-        file { "/var/local/brisskit/backup/source/puppet.sh":
-                ensure  => file,
-                owner   => "root",
-                group   => "backup",
-                mode    => '650',
-                source  => 'puppet:///modules/backup/puppet.sh',
-                require => File["/var/local/brisskit/backup/source"],
-        }
+	case $hostname
+	{
+		ga-backup:
+		{
+		        #puppet_master.sh script
+        		file { "/var/local/brisskit/backup/source/puppet_master.sh":
+                		ensure  => file,
+                		owner   => "root",
+                		group   => "backup",
+                		mode    => '650',
+                		source  => 'puppet:///modules/backup/ga/puppet_master.sh',
+                		require => File["/var/local/brisskit/backup/source"],
+        		}	
 
+	        	#global_master.sh script
+        		file { "/var/local/brisskit/backup/source/global_master.sh":
+                		ensure  => file,
+                		owner   => "root",
+                		group   => "backup",
+                		mode    => '650',
+                		source  => 'puppet:///modules/backup/customer/global_master.sh',
+                		require => File["/var/local/brisskit/backup/source"],
+        		}
+		}
 
-
-
+		ga-puppet:
+		{
+                       #puppet.sh script
+                        file { "/var/local/brisskit/backup/source/puppet.sh":
+                                ensure  => file,
+                                owner   => "root",
+                                group   => "backup",
+                                mode    => '650',
+                                source  => 'puppet:///modules/backup/ga/puppet.sh',
+                                require => File["/var/local/brisskit/backup/source"],
+                        }
+		}
+	}
 }
