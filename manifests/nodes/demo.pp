cda7d401a3f173d48ebcd670de993482b522fe20
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-08-07 16:13:54 +0000

    added demo to customer backup

--- manifests/nodes/demo.pp
+++ manifests/nodes/demo.pp
@@ -6,6 +6,14 @@
 node 'demo-camp.brisskit.le.ac.uk' {
         include bru_base
         include users::jl99, users::rcf8, users::si84, users::ss727, users::tb143
+
+	#Backup stuff
+        include backup::base                                         #Set up file tree
+        include backup::users::vapp_backup                           #Set up users
+        ssh::auth::server { "master_backup": user => "vapp_backup" } #Copy master_backup pub key to vapp_backup authorized_keys
+        ssh::auth::client { "vapp_backup": }                         #Get vapp_backup private key
+
+	package {'mysql-client': ensure => present}
 }
 
 #catissue
@@ -15,6 +23,13 @@ node 'demo-catissue.brisskit.le.ac.uk' {
         include users::integration, users::jl99, users::si84, users::ss727
 	ssh::auth::client { "integration": }
         ssh::auth::server { "integration": }
+
+	#Backup stuff
+        include backup::base                                     #Set up file tree
+        include backup::users::vm_backup                         #Set up users
+        ssh::auth::server { "vapp_backup": user => "vm_backup" } #Copy vapp_backup pub key to vm_backup authorized_keys
+
+	package {'mysql-client': ensure => present}
 }
 
 #civicrm
@@ -22,6 +37,13 @@ node 'demo-civicrm.brisskit.le.ac.uk' {
         include bru_base
         include postfix
         include users::jl99, users::rcf8, users::si84, users::ss727, users::tb143
+
+	#Backup stuff
+        include backup::base                                     #Set up file tree
+        include backup::users::vm_backup                         #Set up users
+        ssh::auth::server { "vapp_backup": user => "vm_backup" } #Copy vapp_backup pub key to vm_backup authorized_keys
+
+	package {'mysql-client': ensure => present}
 }
 
 #i2b2
@@ -37,6 +59,13 @@ node 'demo-i2b2.brisskit.le.ac.uk' {
 node 'demo-onyx.brisskit.le.ac.uk' {
         include bru_base
         include users::jl99
+
+        #Backup stuff
+        include backup::base                                     #Set up file tree
+        include backup::users::vm_backup                         #Set up users
+        ssh::auth::server { "vapp_backup": user => "vm_backup" } #Copy vapp_backup pub key to vm_backup authorized_keys
+
+	package {'mysql-client': ensure => present}
 }
 
 #any other ones that have been missed out
