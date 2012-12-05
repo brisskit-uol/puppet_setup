28ed5f1895c9528b933a8bd93ad02209483050d4
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-12-05 12:05:00 +0000

    added russ as a user

--- modules/users/manifests/jl99.pp
+++ modules/users/manifests/jl99.pp
@@ -4,7 +4,8 @@ class users::jl99{
 	#Users - The password hash is taken from /etc/shadow on the gimp VM. UGLY!
 	#########################################################################
 	#Jeff
-        $jl99_pw='$6$sXsz31a8$oUGK6LDFEYCJQtDLDdDm7U4AJHcHXBCFJB3Xa49NXkhqpsh0m8oAurl5EfZpygDHVM8RgHAyFe4UJ5WLh6pxN/'
+	$jl99_pw='$6$sUDIQM9G$CmOlasR//teeG3vrJVKdlhYk425GtiZYG2oSATdOk6LRn2DBevYf3nM8a8VK68oCignwWz9rXMvae.pU2Kwds0'
+#        $jl99_pw='$6$sXsz31a8$oUGK6LDFEYCJQtDLDdDm7U4AJHcHXBCFJB3Xa49NXkhqpsh0m8oAurl5EfZpygDHVM8RgHAyFe4UJ5WLh6pxN/'
         user { "jl99":
                 ensure     => present,
                 shell      => "/bin/bash",
