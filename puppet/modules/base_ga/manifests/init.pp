e0efcdedc69dad36f8c1fd05f694d9d17436100f
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-12-10 16:29:41 +0000

    moving bases

--- puppet/modules/base_ga/manifests/init.pp
+++ puppet/modules/base_ga/manifests/init.pp
@@ -1,5 +1,15 @@
+#Base class for all the global admin VMs
 class base_ga {
 
 	require base_global
  
+
+	#hosts file
+	file { "/etc/hosts":
+		ensure => present,
+		owner  => 'root',
+		group  => 'root',
+		mode   => 0644,
+		source => "puppet:///modules/base_ga/ga_hosts",
+	}
 }
