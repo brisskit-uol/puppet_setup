de421e0b82cfe0563284809459283c62ea7f189d
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-12-12 13:21:45 +0000

    Added initial Nagios configuration

--- puppet/modules/nagios/manifests/init.pp
+++ puppet/modules/nagios/manifests/init.pp
@@ -1,4 +1,7 @@
 class nagios {
+
+	include nagios::commands
+
 	package { "nagios3":
 		ensure => installed,
 	}
@@ -17,12 +20,55 @@ class nagios {
 		require		=> Package[nagios3],
 	}
 
-	file { "/etc/nagios3/htpasswd.users":
-		notify	=> Service["apache2"],
-		ensure	=> present,
-		owner	=> 'root',
-		group	=> 'root',
-		mode	=> 0644,
-		source	=> "puppet:///modules/nagios/htpasswd.users",
+	exec { "nagios-config-check":
+		command		=> "/usr/sbin/nagios3 -v /etc/nagios3/nagios.cfg && /usr/sbin/service nagios3 reload",
+		refreshonly	=> true,
 	}
+
+#	file { "/etc/nagios3/htpasswd.users":
+#		notify	=> Service["apache2"],
+#		ensure	=> present,
+#		owner	=> 'root',
+#		group	=> 'root',
+#		mode	=> 0644,
+#		source	=> "puppet:///modules/nagios/htpasswd.users",
+#	}
+
+	file { "/var/local/brisskit/nagios-server":
+		ensure	=> directory,
+		owner	=> 'root',
+		group	=> 'nagios',
+		mode	=> 0644,
+	}
+
+	# Remove default configuration files
+	
+	file { [ "/etc/nagios3/conf.d/contacts_nagios2.cfg",
+		"/etc/nagios3/conf.d/extinfo_nagios2.cfg",
+		"/etc/nagios3/conf.d/generic-host_nagios2.cfg",
+		"/etc/nagios3/conf.d/generic-service_nagios2.cfg",
+		"/etc/nagios3/conf.d/hostgroups_nagios2.cfg",
+		"/etc/nagios3/conf.d/localhost_nagios2.cfg",
+		"/etc/nagios3/conf.d/services_nagios2.cfg",
+		"/etc/nagios3/conf.d/timeperiods_nagios2.cfg"
+		]:
+		ensure 	=> absent,
+		force	=> true,
+		notify	=> Service["nagios3"],
+	}
+
+	define nagios-config() {
+		file { "/etc/nagios3/${name}":
+			source	=> "puppet:///modules/nagios/${name}",
+			require	=> Package["nagios3"],
+			notify	=> Exec["nagios-config-check"],
+		}
+	}
+
+	nagios-config { [ "apache2.conf",
+		"cgi.cfg",
+		"commands.cfg",
+		"htpasswd.users",
+		"nagios.cfg",
+		"resource.cfg" ]: }
 }
