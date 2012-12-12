a7d646ce0606ed029ac5ddc407eef75f19bb5d98
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2012-12-12 13:21:45 +0000

    Added initial Nagios configuration

---
+++ modules/nagios/files/notify_by_pushover.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# This file is managed by Puppet
+
+#	Notify by Pushover
+#	by Jedda Wignall
+#	http://jedda.me
+
+#	v1.1 - 02 Dec 2012
+#	Added notification sounds.
+
+#	v1.0 - 21 Aug 2012
+#	Initial release.
+
+#	This script sends a Pushover (http://pushover.net/) notification to your account. I use it in a Nagios setup
+#	to send network monitoring notifications, but it could be used or adapted for nearly any scenario.
+
+#	IMPORTANT
+#	You will need to create a Pushover 'Application' for this script in your account, and use the provided API key
+#	as an argument. You can register an app once logged into Pushover at the follwing link:
+#	https://pushover.net/apps/build
+
+# 	Takes the following REQUIRED arguments:
+
+# 	-u		Your Pushover user key.
+# 	-a		Your Pushover application key.
+# 	-t		The notification title.
+# 	-m		The notification body.
+
+# 	and the following OPTIONAL arguments:
+
+# 	-p		Notification priority. Set to 1 to ignore quiet times.
+# 	-s		Notification sound. You must use one of the parameters listed at https://pushover.net/api#sounds.
+
+# 	Example:
+#	./notify_by_pushover.sh -u r5j7mjYjd -a noZ9KuR5T -s 'spacealarm' -t "server.pretendco.com" -m "DISK WARNING - free space: /dev/disk0s2 4784 MB"
+
+while getopts "u:a:t:m:p:s:" optionName; do
+case "$optionName" in
+u) userKey=( "$OPTARG" );;
+a) appToken=( "$OPTARG" );;
+t) title=( "$OPTARG" );;
+m) message=( "$OPTARG" );;
+p) priority=( "$OPTARG" );;
+s) sound=( "$OPTARG" );;
+
+esac
+done
+
+if [ "$priority" != "" ]; then
+	priorityString="priority=$priority"
+else
+	priorityString="priority=0"
+fi
+
+curl -F "token=$appToken" \
+-F "user=$userKey" \
+-F "title=$title" \
+-F "message=$message" \
+-F "sound=$sound" \
+-F "$priorityString" \
+https://api.pushover.net/1/messages
+
+exit 0
--- modules/nagios/manifests/commands.pp
+++ modules/nagios/manifests/commands.pp
@@ -1,8 +1,25 @@
 class nagios::commands {
 
 	# Command definitions for Nagios
-	# Used to define notification commands such as email/Pushover
+	# Used to define notification commands such as email and Pushover
 
-	
+#	file { "/etc/nagios3/commands.cfg":
+#		ensure	=> file,
+#		owner	=> 'root',
+#		group	=> 'root',
+#		mode	=> 0644,
+#		source	=> "puppet:///modules/nagios/commands.cfg",
+#		require	=> Package["nagios3"],
+#		notify	=> Exec["nagios-config-check"],
+#	}
+
+	file { "/var/local/brisskit/nagios-server/notify_by_pushover.sh":
+		ensure	=> file,
+		owner	=> 'root',
+		group	=> 'nagios',
+		mode	=> 0755,
+		source	=> "puppet:///modules/nagios/notify_by_pushover.sh",
+		require	=> File["/var/local/brisskit/nagios-server"],
+	}
 
 }
