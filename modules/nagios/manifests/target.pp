7791ec0990750580de4aea5d8a0cd43c854ed4fd
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-01-03 14:13:54 +0000

    Moved Nagios client setup to target.pp

--- modules/nagios/manifests/target.pp
+++ modules/nagios/manifests/target.pp
@@ -1,4 +1,34 @@
 class nagios::target {
+
+	package { "nagios-plugins":
+		ensure	=> installed,
+	}
+
+	file { "/var/local/brisskit/nagios-client":
+		ensure	=> directory,
+		owner	=> "root",
+		group	=> "root",
+		mode	=> 0644,
+	}
+
+	file { "/var/local/brisskit/nagios-client/send_nrdp.sh":
+		ensure	=> present,
+		owner	=> "root",
+		group	=> "root",
+		mode	=> 0755,
+		source	=> "puppet:///modules/nagios/send_nrdp.sh",
+		require	=> File["/var/local/brisskit/nagios-client"],
+	}
+
+	file { "/etc/nagios-plugins/config/brisskit.cfg":
+		ensure	=> present,
+		owner	=> "root",
+		group	=> "root",
+		mode	=> 0644,
+		source	=> "puppet:///modules/nagios/brisskit.cfg",
+		require => Package["nagios-plugins"],
+	}
+
 	@@nagios_host { $fqdn:
 		ensure  => present,
 		alias   => $hostname,
