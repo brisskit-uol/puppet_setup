4c2c9a9f75badc32d02dbf8374a9836e6710f3b9
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-01-17 15:30:13 +0000

    Added server specific checks and tidied up many things

---
+++ puppet/modules/nagios/files/check_host.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+host=$(/bin/hostname --fqdn)
+check=$(/usr/bin/uptime)
+rc=$?
+token="token1234"
+url=http://192.168.0.221/nrdp/
+
+
+echo -e "$host\t$rc\t$check" | /var/local/brisskit/nagios-client/send_nrdp.sh -u $url -t $token
+
