e4314ca07171d1c4c6f0bc33b90a3714f11ed9ff
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-02-06 13:06:37 +0000

    Added global and customer classes to host module

---
+++ puppet/modules/hosts/manifests/global.pp
@@ -0,0 +1,13 @@
+class hosts::global {
+
+	require hosts
+
+	$parts = split($fqdn, '[.-]')
+	$parts_hostname = split($fqdn, '[.]')
+	$vapp_name = $parts[0]
+	$vm_role = $parts[1]
+	$vm_name = $parts_hostname[0]
+
+	Host <<| tag == "${vapp_name}" |>> 
+
+}
--- puppet/modules/hosts/manifests/init.pp
+++ puppet/modules/hosts/manifests/init.pp
@@ -1,15 +1,23 @@
 class hosts {
 
-	$parts = split("${fqdn}", '.-')
+	$parts = split($fqdn, '[.-]')
+	$parts_hostname = split($fqdn, '[.]')
 	$vapp_name = $parts[0]
 	$vm_role = $parts[1]
+	$vm_name = $parts_hostname[0]
+
+	host { "localhost":
+		ensure		=> present,
+		ip		=> "127.0.0.1",
+		target		=> "/tmp/hosts_test2",
+	}
 
 	@@host { $fqdn:
 		ensure		=> present,
 		ip		=> $ipaddress,
-		host_aliases	=> $vm_role,
-		tag		=> $vapp_name,
+		host_aliases	=> [ "${vm_name}", "${vm_role}", ],
+		tag		=> "${vapp_name}",
+		target		=> "/tmp/hosts_test2",
 	}
 
-	Host <<| tag == $vapp_name |>>
 }
