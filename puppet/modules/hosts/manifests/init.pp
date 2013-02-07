2970012fd89c1cc4522a9897afefcfff3e0f8448
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-02-07 15:00:33 +0000

    Worked around puppet feature and got host file generation working

--- puppet/modules/hosts/manifests/init.pp
+++ puppet/modules/hosts/manifests/init.pp
@@ -1,23 +1,17 @@
 class hosts {
 
-	$parts = split($fqdn, '[.-]')
-	$parts_hostname = split($fqdn, '[.]')
-	$vapp_name = $parts[0]
-	$vm_role = $parts[1]
-	$vm_name = $parts_hostname[0]
-
 	host { "localhost":
 		ensure		=> present,
 		ip		=> "127.0.0.1",
-		target		=> "/tmp/hosts_test2",
+		#target		=> "/tmp/hosts_test2",
 	}
 
 	@@host { $fqdn:
 		ensure		=> present,
 		ip		=> $ipaddress,
-		host_aliases	=> [ "${vm_name}", "${vm_role}", ],
+		host_aliases	=> ["${hostname}","${vm_role}",],
 		tag		=> "${vapp_name}",
-		target		=> "/tmp/hosts_test2",
+		#target		=> "/tmp/hosts_test2",
 	}
 
 }
