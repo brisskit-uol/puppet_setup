ac5698e8c8cfdf9f0e7ca3c0126b9d0c8f768b2a
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-02-27 09:44:32 +0000

    Removed hosts from manifests

--- manifests/nodes/russ.pp
+++ manifests/nodes/russ.pp
@@ -5,20 +5,17 @@
 #client1
 node 'russ-client1.brisskit.le.ac.uk' {
 	
-	include hosts::customer
 
 }
 
 #client-2
 node 'russ-client-2.brisskit.le.ac.uk' {
 
-	include hosts::customer
 
 }
 
 #master
 node 'russ-master.brisskit.le.ac.uk' {
 
-	include hosts::customer
 
 }
