088231cc3efaf995464e59ca29f3a773f05bcd91
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-02-27 17:00:44 +0000

    added eduserv-specific puppet.conf

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,19 +1,35 @@
 class puppet {
 
+	#Main puppet software
 	service { "puppet":
 		ensure     => "running",
 		hasrestart => true,
 	}
 
-
-
 	#Manage the puppet config file
+	#Note the environment variable.
 	file { "/etc/puppet/puppet.conf":
 		notify => Service["puppet"],
 		ensure => present,
 		owner  => 'root',
 		group  => 'root',
 		mode   => 0644,
-		source => "puppet:///modules/puppet/puppet.conf",
+		source => "puppet:///modules/puppet/puppet.conf.${environment}",
 	}
+
+
+#notify{"${environment}":}
+
+
+	#Need to double check we have a valid environment
+	case "${environment}"
+	{
+		eduserv:
+		{
+			notify{"Eduserv!":}
+		}
+
+	}
+
+
 }
