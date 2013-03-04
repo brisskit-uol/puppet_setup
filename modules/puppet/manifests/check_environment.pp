84342f6d10f0cd3aedc412c888c49f9fc308accf
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-03-04 15:51:14 +0000

    split check env into separate file

---
+++ modules/puppet/manifests/check_environment.pp
@@ -0,0 +1,21 @@
+class puppet::check_environment {
+
+	#Need to double check we have a valid environment
+	case "${environment}"
+	{
+		eduserv:
+		{
+			notify{"Eduserv!":}
+		}
+
+		uhl:
+		{
+			notify{"UHL!":}
+		}
+
+		default:
+		{
+			notify{"Bad environment set! I have ${environment}";}
+		}
+	}
+}
