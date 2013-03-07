82292feefbb826d4f8826f59c75a957adf924a88
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-03-07 11:40:16 +0000

    Added UHL IP address info. Corrected a few typos

--- puppet/modules/fw/manifests/target/backup.pp
+++ puppet/modules/fw/manifests/target/backup.pp
@@ -22,19 +22,28 @@ class fw::target::backup {
 
 		"uhl": {
 
-			# Requires network address information
+			# Requires multiple rules to cater for multiple
+			# CIDR blocks
 
 			#################################
 			# Allow SSH from customer vApps	#
 			#################################
 
-			#firewall { "110 allow SSH from INTERNAL":
+			firewall { "110 allow SSH from INTERNAL":
 
-				#source	=> "192.168.2.0/24",
-				#port	=> "22",
-				#action	=> "accept",
+				source	=> "10.228.174.176/28",
+				port	=> "22",
+				action	=> "accept",
 
-			#}
+			}
+
+			firewall { "110 allow SSH from INTERNAL":
+
+				source	=> "10.228.174.192/28",
+				port	=> "22",
+				action	=> "accept",
+
+			}
 
 		}
 
