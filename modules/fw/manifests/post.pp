89bab1af22c1125f9e1284b8f095e9b441c35c9e
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-03-20 15:05:53 +0000

    Added logging for dropped packets

--- modules/fw/manifests/post.pp
+++ modules/fw/manifests/post.pp
@@ -1,5 +1,13 @@
 class fw::post {
 
+	firewall { '998 log all':
+		proto		=> 'all',
+		jump		=> 'LOG',
+		log_prefix	=> "[IPTABLES] dropped ",
+		limit		=> "2/min",
+		before		=> undef,
+	}
+
         firewall { '999 drop all':
                 proto   => 'all',
                 action  => 'drop',
