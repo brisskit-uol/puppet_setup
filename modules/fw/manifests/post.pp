bf8d00107eb85aab1e88112be4bbdebfe645e6ff
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-03-21 15:00:45 +0000

    Added more comments. Added default Firewall type to post to prevent dependency cycle

--- modules/fw/manifests/post.pp
+++ modules/fw/manifests/post.pp
@@ -1,17 +1,30 @@
 class fw::post {
+	
+	# Set default before to undefined to prevent dependency cycle
+
+	Firewall {
+
+		before	=> undef,
+
+	}
+
+        # Default firewall rules
+        # These will be applied *last* and to all nodes
+
+	# Log all dropped traffic
 
 	firewall { '998 log all':
 		proto		=> 'all',
 		jump		=> 'LOG',
 		log_prefix	=> "[IPTABLES] dropped ",
 		limit		=> "2/min",
-		before		=> undef,
 	}
+
+	# Drop all unmatched traffic
 
         firewall { '999 drop all':
                 proto   => 'all',
                 action  => 'drop',
-                before  => undef,
         }
 
 }
