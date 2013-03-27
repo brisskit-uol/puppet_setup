4003881cae3a3c9f23cda1113ee21f9a834ce4bd
rp310 <rp310@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-03-27 10:38:52 +0000

    Modified maven rules to be correct. Added rules for pub servers

--- modules/fw/manifests/target/maven.pp
+++ modules/fw/manifests/target/maven.pp
@@ -2,9 +2,19 @@ class fw::target::maven {
 
 	require fw::target::ga
 
-	firewall { "505 allow www from ANY":
+	firewall { "505 redirect www to 8081":
 
-		port	=> "80",
+		dport	=> "80",
+		table	=> "nat",
+		chain	=> "PREROUTING",
+		jump	=> "REDIRECT",
+		toports	=> "8081",
+		
+	}
+
+	firewall { "505 allow 8081 from ANY":
+
+		port	=> "8081",
 		action	=> "accept",
 
 	}
