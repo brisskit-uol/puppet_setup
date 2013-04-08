fd44acb62d07070791a66846f4a63d0734301d9a
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-04-08 15:02:07 +0000

    added third stage

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,6 +1,7 @@
 #Define some run stages
 stage { 'first': before => Stage['second'] }
-stage { 'second': before => Stage['main'] }
+stage { 'second': before => Stage['third'] }
+stage { 'third': before => Stage['main'] }
 
 #Set up all the common ssh keys
 import "keys"
