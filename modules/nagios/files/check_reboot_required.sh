2d079d122fef42bbda68c62130f49ce9213c81f2
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-04-09 10:16:46 +0000

    added a nagios check to see if a reboot is needed

---
+++ modules/nagios/files/check_reboot_required.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sleepseconds=120
+sleeptime=$(/usr/bin/facter macaddress | tr -cd [:digit:] | awk '{printf "%d\n",$1}')
+let sleeptime%=$sleepseconds
+sleep $sleeptime
+
+host=$(/usr/bin/facter fqdn)
+check=$(/usr/lib/nagios/plugins/check_reboot_required)
+
+rc=$?
+service_name="Reboot required"
+token="mcn4y86v1bqvhup47w2r4c5d7hqx7ycx"
+url=http://ga-nagios/nrdp/
+
+
+echo -e "$host\t$service_name\t$rc\t$check" | /var/local/brisskit/nagios-client/send_nrdp.sh -u $url -t $token
+
