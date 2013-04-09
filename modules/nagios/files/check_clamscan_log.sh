d6f19fcbeb072da4fd66a821d8c8100a517aa3e8
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-04-09 16:52:29 +0000

    added clamscan run and nagios check

---
+++ modules/nagios/files/check_clamscan_log.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sleepseconds=120
+sleeptime=$(/usr/bin/facter macaddress | tr -cd [:digit:] | awk '{printf "%d\n",$1}')
+let sleeptime%=$sleepseconds
+sleep $sleeptime
+
+host=$(/usr/bin/facter fqdn)
+check=$(/usr/lib/nagios/plugins/check_clamscan_log)
+
+rc=$?
+service_name="Clamscan"
+token="823uyrhfksahfjlahsf84ahjkasfd82k"
+url=http://ga-nagios/nrdp/
+
+
+echo -e "$host\t$service_name\t$rc\t$check" | /var/local/brisskit/nagios-client/send_nrdp.sh -u $url -t $token
+
