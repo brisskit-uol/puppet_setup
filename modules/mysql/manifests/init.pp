b1a4917f5666767c237e1547cf47ae998d62ea3a
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-04-18 18:40:35 +0000

    first stab at templating mysql install

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,6 +1,18 @@
 class mysql {
 
-      $pound_ipaddress = pdbfactquery("${vapp_name}-pound.brisskit.le.ac.uk", "ipaddress")
+#      $pound_ipaddress = pdbfactquery("${vapp_name}-pound.brisskit.le.ac.uk", "ipaddress")
+#	$spound_ipaddress = query_nodes('vapp_name="${vapp_name}"','ipaddress')
+
+  $pound_ipaddress = query_nodes('fqdn="bru1-pound.brisskit.le.ac.uk"','ipaddress')
+ $camp_ipaddress = query_nodes('fqdn="bru1-camp.brisskit.le.ac.uk"','ipaddress')
+ $fake_ipaddress = query_facts('fqdn="bru1-fake.brisskit.le.ac.uk"','ipaddress')
+#$fake_ipaddress=undef
+
+
+
+
+notify{"$pound_ipaddress": }
+#inotify{"$fake_ipaddress": }
 
       file { "test":
       	   path    => "/etc/brisskit/mysqltest.conf",
