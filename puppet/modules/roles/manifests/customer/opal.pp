549c7981b67d97d255beb275647bfcbeb67ed3fb
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-04-19 16:05:21 +0000

    added obiba repo to opal role

--- puppet/modules/roles/manifests/customer/opal.pp
+++ puppet/modules/roles/manifests/customer/opal.pp
@@ -11,4 +11,18 @@ class roles::customer::opal {
 	# Create nagios cfg file for camp role
 	include nagios::target::camp
 
+	
+	#################
+	# Opal!         #
+	#################
+
+	#Add obiba key and url to sources list
+	apt::source { 'obiba':
+		location   => 'http://pkg.obiba.org',
+		release    => 'stable/',
+		repos      => '',
+		key_source => 'http://pkg.obiba.org/obiba.org.key',
+		include_src => false,
+	}
+
 }
