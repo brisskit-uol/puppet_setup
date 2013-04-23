63533d8060c852b60d4bb8a5f310bb597b26c495
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-04-23 16:49:49 +0000

    moved to a query based hosts lookup

--- puppet/modules/hosts/manifests/init.pp
+++ puppet/modules/hosts/manifests/init.pp
@@ -1,4 +1,5 @@
-# Manages host file entries
+#Manages host file entries.
+#Im only exporting and collecting ga and pub since the customer vapps need a lot more care and attention.
 
 class hosts {
 
@@ -9,32 +10,29 @@ class hosts {
 		ip		=> "127.0.0.1",
 	}
 
-	# Export entry for fqdn with aliases to PuppetDB and tag with vApp name
+	#Export entry for fqdn with aliases to PuppetDB and tag with vApp name for ga and pub vapps
+	if $vapp_name == 'ga' or $vapp_name == 'pub'
+	{
+		case $vm_role {
 
-	case $vm_role {
-
-		"maven", "maven2": {
-
-			@@host { $fqdn:
-				ensure		=> present,
-				ip		=> $ipaddress,
-				host_aliases	=> ["maven.brisskit.org","${vm_role}.brisskit.org","${hostname}","${vm_role}",],
-				tag		=> "${vapp_name}",
+			"maven", "maven2": {
+				@@host { $fqdn:
+					ensure		=> present,
+					ip		=> $ipaddress,
+					host_aliases	=> ["maven.brisskit.org","${vm_role}.brisskit.org","${hostname}","${vm_role}",],
+					tag		=> "${vapp_name}",
+				}
 			}
 
-		}
-
-		default: {
-
-			@@host { $fqdn:
-				ensure		=> present,
-				ip		=> $ipaddress,
-				host_aliases	=> ["${hostname}","${vm_role}",],
-				tag		=> "${vapp_name}",
+			default: {
+				@@host { $fqdn:
+					ensure		=> present,
+					ip		=> $ipaddress,
+					host_aliases	=> ["${hostname}","${vm_role}",],
+					tag		=> "${vapp_name}",
+				}
 			}
-
 		}
 
 	}
-
 }
