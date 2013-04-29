16b018966e329028b2a59d539c298cdbf9b17846
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-04-29 15:41:50 +0000

    moded virtual users in realise

--- modules/users/manifests/global.pp
+++ modules/users/manifests/global.pp
@@ -1,6 +1,8 @@
 #Global sysadmins. These guys get put on ALL VMs.
 class users::global
 {
+	require users::virtual
+
         realize( Users::Virtual::Ssh_user["ob30"] )
         realize( Users::Virtual::Ssh_user["rp310"] )
 }
