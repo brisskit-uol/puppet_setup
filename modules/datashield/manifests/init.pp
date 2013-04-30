f981b187845f306c6abee8e4311f09cccc0c06ce
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-04-30 16:15:54 +0000

    added rserve user creation

--- modules/datashield/manifests/init.pp
+++ modules/datashield/manifests/init.pp
@@ -1,27 +1,33 @@
 #Add datashield to the opal install
-datashield {
+class datashield {
 
 	#Need to check opal is installed and running
 
 
 	#Install R components
 	package { 'r-base':
-		ensure => installed
+		ensure => installed,
 	}
 	
 	package { 'r-cran-rserve':
 		ensure => installed,
-		require => Package['r-base']
+		require => Package['r-base'],
 	}
 
 
 	#Copy init script across
-	File { '/etc/init.d/rserve':
+	file { '/etc/init.d/rserve':
 		ensure => present,
 		owner  => root,
 		group  => root,
 		mode   => 755,
-		source => 'puppet://modules/datashield/init.txt',
+		source => 'puppet:///modules/datashield/init.txt',
+	}
 
+	#Add a user that rserv runs as - this is set in the init.d script
+	user { 'rserve':
+		ensure  => present,
+		home    => '/var/lib/rserve',
+		managehome => true,
 	}
 }
