255c16f57b85090414a0cbe3db9c1004ccd630d3
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-05-23 09:31:56 +0000

    added opal nagios check

---
+++ puppet/modules/nagios/manifests/target/opal.pp
@@ -0,0 +1,13 @@
+class nagios::target::onyx {
+
+	require nagios::target
+
+	@@nagios_service { "check-http-opal-${fqdn}":
+		use			=> "check-http-active",
+		host_name		=> $fqdn,
+		servicegroups		=> "check-http-opal",
+		target			=> "/etc/nagios3/conf.d/service_${fqdn}.cfg",
+		check_command		=> "check-http-pound!${vapp_name}!/opal",
+		service_description	=> "HTTP Opal",
+	}
+}
