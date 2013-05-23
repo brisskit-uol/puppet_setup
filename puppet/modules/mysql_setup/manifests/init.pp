c2deb45116e921aea5fc19fcedcb3c1944256e57
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-05-23 15:42:57 +0000

    lots of mysql puppet dev

--- puppet/modules/mysql_setup/manifests/init.pp
+++ puppet/modules/mysql_setup/manifests/init.pp
@@ -9,51 +9,129 @@ class mysql_setup {
 	}
 
 
-#	mysql::db { 'redcap':
- #               user     => 'redcap',
-  #              password => 'mypass',
-   #             host     => 'redcap',
-    #            grant    => ['all'],
-     #   }
+	mysql_setup::add_user {'jim': 
+		database => 'test2',
+		user => 'jim',
+		host => 'somehost',
+		ensure => 'absent',
+	}
+
+
+        #catisue
+        mysql_setup::add_user {'catissue':
+                database => 'catissue',
+                user     => 'catissue',
+                host     => 'catissue',
+        }
+
+        #civicrm
+        mysql_setup::add_user {'civicrm':
+                database => 'civicrm',
+                user     => 'civicrm',
+                host     => 'civicrm',
+        }
+
+        #onyx
+        mysql_setup::add_user {'onyx':
+                database => 'onyx',
+                user     => 'onyx',
+                host     => 'onyx',
+        }
+
+        #opal
+        mysql_setup::add_user {'opal':
+                database => 'opal',
+                user     => 'opal',
+                host     => 'opal',
+        }
 
 
 	#redcap
-	mysql_setup::app_db {'redcap':
-		database => 'redcap',
-		user     => 'redcap',
-		host     => 'redcap',
-	}
+	mysql_setup::add_user {'redcap':
+                database => 'redcap',
+                user     => 'redcap',
+                host     => 'redcap',
+        }
 
-	#civicrm
-	mysql_setup::app_db {'civicrm':
-		database => 'civicrm',
-		user     => 'civicrm',
-		host     => 'civicrm',
-	}
 
 
 }
 
-#Set up the database and output the password to a file.
-define mysql_setup::app_db ($database, $user, $host) {
+
+#Set up the DB, the user, and its grants. The host variable is the ROLE the VM has.
+define mysql_setup::add_user ($database, $user, $host, $grant = 'all', $ensure = 'present') {
+
+
+	#Make the DB. 
+        database {"$database":
+                ensure => "$ensure",
+        }
+
+
+	#$temp=loadyaml("/etc/brisskit/mysql/$user.cfg")
+
+	file { "/etc/brisskit/mysql/$user.yaml":
+		ensure => 'present',
+	#	source => "puppet:///modules/mysql/$user.yaml",
+	}
 
 
 	#Figure out password
-	#I could set this here, or I could read it in from somewhere
-	$password="huh"
+        #I could set this here, or I could read it in from somewhere
+        $password="huh"
 
-	#Print password to file
-	$pw_string = 	"#Database config params set by puppet\nhost=mysql\ntype=mysql\ndatabase=$database\nuser=$user\npassword=$password"
-	file { "/etc/brisskit/mysql/$user.cfg":
+        #Print password to file
+        $pw_string =    "#Database config params set by puppet\nhost=mysql\ntype=mysql\ndatabase=$database\nuser=$user\npassword=$password"
+        file { "/etc/brisskit/mysql/$user.cfg":
                 content => "$pw_string",
         }
 
 
-	#Make the database
-	mysql::db { "$database":
-                user     => "$user",
-                password => "$password",
-                host     => "$host",
-                grant    => ['all'],
+	#Set up the user. Note how this uses three different hostnames, this is because
+	#the various apps connect under different hostnames.
+	
+	#user@shortname
+	database_user {"${user}@${host}":
+		ensure        => "$ensure",
+		password_hash => mysql_password($password),
+	}
+
+        #user@vapp-shortname
+        database_user {"${user}@${vapp_name}-${host}":
+                ensure        => "$ensure",
+                password_hash => mysql_password($password),
+        }
+
+        #user@vapp-shortname.brisskit.le.ac.uk
+        database_user {"${user}@${vapp_name}-${host}.brisskit.le.ac.uk":
+                ensure        => "$ensure",
+                password_hash => mysql_password($password),        
+	}
+
+
+	#Do the grants
+  	
+	#user@shortname
+	database_grant {"${user}@${host}/${database}":
+      		privileges => $grant,
+      		provider   => 'mysql',
+      		require    => Database_user["${user}@${host}"],
+    	}
+
+        #user@vapp-shortname
+        database_grant {"${user}@${vapp_name}-${host}/${database}":
+                privileges => $grant,
+                provider   => 'mysql',
+                require    => Database_user["${user}@${vapp_name}-${host}"],
+        }
+
+        #user@vapp-shortname.brisskit.le.ac.uk
+        database_grant {"${user}@${vapp_name}-${host}.brisskit.le.ac.uk/${database}":
+                privileges => $grant,
+                provider   => 'mysql',
+                require    => Database_user["${user}@${vapp_name}-${host}.brisskit.le.ac.uk"],
         }
 }
+
+
+
