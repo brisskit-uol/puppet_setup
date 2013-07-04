6aabe577267f814980f478209297096697754f06
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-07-04 09:52:32 +0000

    added ssh config files to puppet

--- puppet/modules/sshd/manifests/init.pp
+++ puppet/modules/sshd/manifests/init.pp
@@ -1,3 +1,4 @@
+#Manage the sshd install and config
 class sshd {
 
 	package { "openssh-server": 
@@ -7,4 +8,33 @@ class sshd {
 	service { "ssh":
         	ensure => running,
     	}
+
+	#Not really sshd I know, but thsi will stop connections being dropped
+	file { "/etc/ssh/ssh_config":
+                path    => "/etc/ssh/ssh_config",
+                ensure  => present,
+                owner   => root,
+                group   => root,
+                mode    => 644,
+                source => "puppet:///modules/sshd/ssh_config",
+                notify  => Service["ssh"],
+        }
+
+
 }
+
+#Some VMs use keys to log into, others passwords, the
+#config file must reflect that.
+class sshd::config::password {
+
+	file { "/etc/ssh/sshd_config":
+                path    => "/etc/ssh/sshd_config",
+		ensure  => present,
+                owner   => root,
+                group   => root,
+                mode    => 644,
+                source => "puppet:///modules/sshd/sshd_config",
+                notify  => Service["ssh"],
+        }
+}
+
