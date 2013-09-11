bfee8c0c32f8107751d8e8d49559836894f1dab2
ob30 <ob30@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2013-09-11 14:44:06 +0000

    fixed catissuecore url path

--- puppet/modules/nagios/manifests/target/catissue.pp
+++ puppet/modules/nagios/manifests/target/catissue.pp
@@ -7,7 +7,7 @@ class nagios::target::catissue {
 		host_name		=> $fqdn,
 		servicegroups		=> "check-http-catissue",
 		target			=> "/etc/nagios3/conf.d/service_${fqdn}.cfg",
-		check_command		=> "check-http-pound!${vapp_name}!/catissuecore",
+		check_command		=> "check-http-pound!${vapp_name}!/catissuecore/",
 		service_description	=> "HTTP caTissue",
 	}
 }
